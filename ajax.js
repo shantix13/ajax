41f8f7e3283485363a3f35ed4d10f1632c43a191
shantix <shantix@spaceofjoy.ru>
2020-06-14 07:35:49 +0300

    Ajax - requests types/query string

--- ajax.js
+++ ajax.js
@@ -1,7 +1,28 @@
+// $.ajax(
+//     'https://repetitora.net/api/JS/Images',
+//     {
+//         success: function (data) {
+//             data.forEach(el => {
+//                 const img = document.createElement('img');
+//                 img.src = el.thumbnail;
+//                 document.querySelector('body').appendChild(img);
+//             });
+//         }
+//     }
+// );
+// Endpoint - оконечная точка.
+
+// http протокол имеет разные типы запросов. Get / Post / Options / Delete / Put.
+
+// Get - запрос для получения данных. Самый простой запрос. Состоит из одной характеристики - строка запроса. 
+
+// query string - строка запроса. Можно заслать параметры для получения опеределенных данных.
+
+// https://repetitora.net/api/JS/Images/?page=2&click=yes&other=blablabla
 
 
 $.ajax(
-    'https://repetitora.net/api/JS/Images',
+    'https://repetitora.net/api/JS/Images/?page=2&count=4',
     {
         success: function (data) {
             data.forEach(el => {
@@ -11,4 +32,4 @@ $.ajax(
             });
         }
     }
-)
+);
