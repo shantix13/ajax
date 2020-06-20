185d1c270df8d7abecd29ef0beb3b21196797ceb
shantix <shantix@spaceofjoy.ru>
2020-06-20 10:34:24 +0300

    Fetch

--- ajax.js
+++ ajax.js
@@ -1,16 +1,17 @@
-// function getImages(ajaxUrl) {
-//     let promise = $.ajax(ajaxUrl);
-//     //console.log(promise);
-//     return promise;
-// }
+async function getResponse() {
+    let promise = await fetch('https://jsonplaceholder.typicode.com/photos');
+    let content = await promise.json();
+    content = content.splice(0, 10);
+    let posts = document.querySelector('.posts');
+    let post_item = '';
+    for (items of content) {
+        console.log(items);
+        post_item += `<li><img src=${items.url} alt=${items.title} ></li>`;
+    };
+    posts.innerHTML = post_item;
+};
 
-// getImages('https://repetitora.net/api/JS/Images');
+getResponse();
 
-function getImagesAxios(ajaxUrl) {
-    let promise = axios.get(ajaxUrl);
-    return promise.then((data) => {
-        console.log(data.data);
-    });
-}
 
-getImagesAxios('https://repetitora.net/api/JS/Images');
+
