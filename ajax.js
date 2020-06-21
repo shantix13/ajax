3b280c2c0eeb05a2fdba1d7e4577edb94042ee21
shantix <shantix@spaceofjoy.ru>
2020-06-21 10:40:18 +0300

    Fetch & Promises, p.3

--- ajax.js
+++ ajax.js
@@ -1,11 +1,34 @@
-let promise = fetch('https://jsonplaceholder.typicode.com/photos')
-    .then(data => data.text())
-    // data.text().then(data2 => {
-    //     console.log(data2);
-    // });
-    .then(data => {
-        console.log(data);
-    });
+// Объект promise
+// Promise устраняет ад колбеков
+
+let promise = new Promise((resolve, reject) => {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(data => {
+            resolve(data.text());
+        });
+});
+
+let promise2 = new Promise((resolve, reject) => {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(data => {
+            resolve(data.text());
+        });
+});
+
+
+promise.then(data => {
+    console.log(data);
+});
+
+promise2.then(data => {
+    console.log(data);
+});
+
+// PromiseAll - синтакси позволяет обработать обе инстанции объекта вместе.
+// Результат promise записывется в массив и приходят в том порядке, в котороым мы указаваем параметры массива.
+Promise.all([a, b]).then(data => {
+    console.log(data);
+});
 
 
 
